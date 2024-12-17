6db6f3d81ecde6a9ffaddf35a2ecf672a0521bea
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2024-12-17 14:26:41 +0100

    day 8 p 2

--- kotlin/app/src/main/kotlin/dev/rockyj/advent24/Day8.kt
+++ kotlin/app/src/main/kotlin/dev/rockyj/advent24/Day8.kt
@@ -14,7 +14,7 @@ fun main() {
     val height = contents.size
 
     println(part1(matrix, width, height))
-    // println(part2(matrix, width, height))
+    println(part2(matrix, width, height))
 }
 
 private fun findAntinodes(matrix: Map<Pair<Int, Int>, String>, width: Int, height: Int, freq: String): List<Pair<Int, Int>> {
@@ -37,12 +37,53 @@ private fun findAntinodes(matrix: Map<Pair<Int, Int>, String>, width: Int, heigh
     return antiNodes.toList()
 }
 
+private fun findAntinodes2(matrix: Map<Pair<Int, Int>, String>, width: Int, height: Int, freq: String): List<Pair<Int, Int>> {
+    val antennas = matrix.keys.filter { matrix[it] == freq }
+    val antiNodes = mutableListOf<Pair<Int, Int>>()
+
+    antennas.forEach { node1 ->
+        antennas.forEach { node2 ->
+            if (node1 != node2) {
+                // find all points in a line connecting node1 & node2
+                val colDiff = node2.first - node1.first
+                val rowDiff = node2.second - node1.second
+
+                var x = node1.first
+                var y = node1.second
+                var go = true
+
+                while (go) {
+                    antiNodes.plusAssign(Pair(x, y))
+                    x += colDiff
+                    y += rowDiff
+
+                    if (x < 0 || x > width - 1 || y < 0 || y > height -1) {
+                        go = false
+                    }
+                }
+            }
+        }
+    }
+
+    return antiNodes.toList()
+}
+
 private fun part1(matrix: Map<Pair<Int, Int>, String>, width: Int, height: Int): Int {
     val frequencies = matrix.keys.filter { matrix[it] != "." }.map { matrix[it] }.distinct()
 
     val res = frequencies.map { findAntinodes(matrix, width, height, it!!) }.flatten()
 
     // println(res.distinct())
+
+    return res.distinct().size
+}
+
+private fun part2(matrix: Map<Pair<Int, Int>, String>, width: Int, height: Int): Int {
+    val frequencies = matrix.keys.filter { matrix[it] != "." }.map { matrix[it] }.distinct()
+
+    val res = frequencies.map { findAntinodes2(matrix, width, height, it!!) }.flatten()
+
+    // println(res.distinct().sortedBy { it.second })
 
     return res.distinct().size
 }
