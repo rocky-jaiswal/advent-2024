8cc758e3b0d598854055e50cbe11118637d49175
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2024-12-19 14:02:24 +0100

    day 9 p2

--- deno/src/day9.ts
+++ deno/src/day9.ts
@@ -1,4 +1,4 @@
-import { range } from 'ramda';
+import { range, slice } from 'ramda';
 import { readFile } from './utils/readFile.ts';
 
 class DiskFile {
@@ -77,8 +77,43 @@ const flip = (mutStr: string[]) => {
   }
 };
 
+const moveUp = (
+  id: number,
+  spaceNeeded: number,
+  idIsAt: number,
+  mutStr: string[]
+) => {
+  const space = range(0, spaceNeeded)
+    .map((_) => '.')
+    .join('');
+
+  let freeIdx = -1;
+
+  for (let index = 0; index < mutStr.length; index++) {
+    if (slice(index, spaceNeeded + index, mutStr).join('') === space) {
+      freeIdx = index;
+      break;
+    }
+  }
+
+  if (freeIdx !== -1 && freeIdx < idIsAt) {
+    // console.log(freeIdx);
+    // console.log(idIsAt);
+
+    for (let index = 0; index < spaceNeeded; index++) {
+      mutStr[freeIdx + index] = `${id}`;
+    }
+
+    for (let index = 0; index < spaceNeeded; index++) {
+      mutStr[idIsAt + index] = '.';
+    }
+  }
+
+  return mutStr;
+};
+
 const part1 = async () => {
-  const contents = await readFile('resources/day9_2.txt');
+  const contents = await readFile('resources/day9_1.txt');
 
   const nums = contents[0].split('');
   const diskFiles: Array<DiskFile> = [];
@@ -113,4 +148,49 @@ const part1 = async () => {
   return checksum(mutStr);
 };
 
-part1().then(console.log);
+const part2 = async () => {
+  const contents = await readFile('resources/day9_2.txt');
+
+  const nums = contents[0].split('');
+  const diskFiles: Array<DiskFile> = [];
+  let idx = 0;
+
+  nums.forEach((_num: string, index: number) => {
+    if (index % 2 === 0) {
+      const df = new DiskFile(
+        idx,
+        parseInt(nums[index].trim()),
+        parseInt(nums[index + 1] ?? 0)
+      );
+      diskFiles.push(df);
+      idx += 1;
+    }
+  });
+
+  const disk: Array<string> = [];
+  const diskIdSpace: Record<number, number> = {};
+  const diskIdLocn: Record<number, number> = {};
+  const diskIds: Array<number> = [];
+
+  diskFiles.forEach((df) => {
+    diskIds.push(df.id);
+    diskIdSpace[df.id] = df.occSpace;
+    diskIdLocn[df.id] = disk.length;
+
+    range(0, df.occSpace).forEach((_) => disk.push(`${df.id}`));
+    range(0, df.freeSpaceAfter).forEach((_) => disk.push('.'));
+  });
+
+  let mutStr = disk;
+
+  console.log('-------');
+
+  diskIds.reverse().forEach((id: number) => {
+    mutStr = moveUp(id, diskIdSpace[id], diskIdLocn[id], mutStr);
+  });
+
+  return checksum(mutStr);
+};
+
+// part1().then(console.log);
+part2().then(console.log);
